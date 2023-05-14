7e373a78533cb676ba4d6cf024c54ae33d407008
Crowdbotics <team@crowdbotics.com>
2023-05-14 10:53:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-fire-40680",
+  url: "https://late-fire-40680.botics.co",
+  api: "https://late-fire-40680.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
